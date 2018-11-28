7fdad89eedc2aa1abc688aade89232510318920f
ChristianHS23 <christian.sihotang23@gmail.com>
2018-11-28 10:34:47 +0700

    add string to array converter function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //your code here
+function stringtoArray(input){
+    var output=[]
+    let temp=[]
+    for(let i=0; i <= input.length ;i++){
+       
+        if(input[i] == ',' || i == input.length ){
+            output.push(temp)
+            temp = []
+        }else{
+            temp.push(input[i])
+        }
+        
+    }
+    return output
+}
+var input= 'aqrst,ukaei,ffooo'
+console.log(stringtoArray(input))
