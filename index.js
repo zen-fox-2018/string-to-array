b2fae48a330e6b6a6791b1562962799edc9cb55c
ahmad <ahmad.syukron6991@gmail.com>
2018-11-28 12:41:18 +0700

    stringto array done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//your code here
+function stringToarray(str){
+    var cek = str.split(',') 
+    var result = []
+    for(let i = 0; i < cek.length; i ++){
+        var holder = []
+        for(let j = 0; j < cek[i].length; j ++){
+            holder.push(cek[i][j])
+        }
+        result.push(holder)
+    }
+    return result
+}
+console.log(stringToarray('aqrst,ukaei,ffooo'))
+console.log(stringToarray('qwer,tyui,asdf,ghjk'))
