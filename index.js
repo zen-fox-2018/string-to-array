bcd04821206a158ab70ae2ce82a54b2f39f61346
fauzan_bintang <obin.aji@gmail.com>
2018-11-28 10:42:23 +0700

    done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function stringToArray(str) {
     str = str.split(',')
-    let result = []
-    str.forEach(e => {
-        result.push(e.split(''))
-    });
-    return result
+    // let result = []
+    // str.forEach(e => {
+    //     result.push(e.split(''))
+    // });
+    return str.map(e => {
+        return e.split('')
+    })
+    // return result
 }
 
 console.log(stringToArray('aqrst,ukaei,ffooo'));
