2c6a954351b94d88755c2a358cc302ac8c008b8b
Luthfi Arfiansyah <luthfi.arfi@gmail.com>
2018-11-28 11:20:35 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //your code here
+//your code here
+function stringToArray(str) {
+  var arrStr = str.split(',')
+  var result = [];
+  for (var i = 0; i < arrStr.length; i++) {
+    var innerResult = [];
+    for (var j = 0; j < arrStr[i].length; j++) {
+      innerResult.push(arrStr[i][j]);
+    }
+    result.push(innerResult);
+  }
+  return result;
+}
+
+console.log(stringToArray('abcd,aged,just'));
