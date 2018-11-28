fef1a0f22c6b4fb28cc58a3ccf2f3f8654710156
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-11-28 10:23:34 +0700

    submit task

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-//your code here
+function stringArr (input) {
+  var split = [];
+  var tempKata = []
+
+  for (var i = 0; i < input.length; i++) {
+    if (input[i] !== ',') {
+      tempKata.push(input[i]);
+    } else if (input[i] === ',') {
+      split.push(tempKata);
+      tempKata = [];
+    }
+  }
+  if (tempKata.length > 0) {
+    split.push(tempKata);
+  }
+  return split;
+}
+
+console.log(stringArr("aqrst,ukaei,ffooo"));
+console.log(stringArr("qwer,tyui,asdf,ghfj"));
