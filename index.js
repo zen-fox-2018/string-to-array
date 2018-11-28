dd3cf96412cfdd2dbf4bb390475b7ec0048dc9cf
ACER <fauziaazia171097@gmail.com>
2018-11-28 03:10:49 -0500

    finish

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //your code here
+function convertStringtoArr(input) {
+    let arr = input.split(',')
+    let result = []
+    arr.forEach(element => {
+        let arr = []
+        for (let j = 0 ; j < element.length ; j++) {
+            arr.push(element[j])
+        }
+        result.push(arr)
+    });
+    return result
+}
+
+let kata = "aqrst,ukaei,ffooo"
+let kata2 = "qwer,tyui,asdf,ghjk"
+console.log(convertStringtoArr(kata))
+console.log(convertStringtoArr(kata2))
