770bbe09afaefe528c8c35a891db042a04390ee2
ShadowM8 <anton2014lenovo@gmail.com>
2018-11-28 11:05:57 +0700

    stringToArray done -anton-

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//your code here
+function stringToArray(string) {
+    let arrayInput = string.split(',')
+    let hasil = []
+    for (let i = 0; i < arrayInput.length; i++) {
+        let hasilDalam = []
+        for (let j = 0; j < arrayInput[i].length; j++){
+            hasilDalam.push(arrayInput[i][j])
+        }
+        hasil.push(hasilDalam)
+    }
+    return hasil
+}
+
+console.log(stringToArray('aqrst,ukaei,ffooo'))
+console.log(stringToArray('qwer,tyui,asdf,ghjk'))
