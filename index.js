1d8c402b3f788283b1974a125c880c5aaa7b25ec
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-11-28 14:05:40 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-//your code here
+function stringArray(input) {
+  return input.split(',').map(x => x.split(''));
+}
+
+let input1 = 'aqrst,ukaei,ffooo'
+let input2 = 'qwer,tyui,asdf,ghjk'
+
+console.log(stringArray(input1));
+console.log(stringArray(input2));
