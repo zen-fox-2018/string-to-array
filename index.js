6dec5df0a396a757a126bb587b30dfa92ba0a5d3
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-28 10:34:39 +0700

    komit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-//your code here
+function generate(string) {
+    let str = string.split(`,`)
+    let array = []
+    let arrayTemp = []
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < str[0].length; j++) {
+            arrayTemp.push(str[i][j])
+        }
+        array.push(arrayTemp)
+        arrayTemp = []
+        
+    }
+    return array
+    
+}
+
+console.log(generate(`taqi,aduh,aziz,tiga`));
+
