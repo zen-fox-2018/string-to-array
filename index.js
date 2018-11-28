9886413d26840d340f9cffca74f52b81a6683223
rachmadza <razato.zai@gmail.com>
2018-11-28 11:39:08 +0700

    push anchor string to array

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //your code here
+function stringToArray(input) {
+    let output = []
+    let pisah = input.split(',')
+    // console.log(pisah);
+    
+    for (let i = 0; i < pisah.length; i++) {
+        let box = []
+        for (let j = 0; j < pisah[i].length; j++) {
+            box.push(pisah[i][j])
+            
+        }
+        output.push(box)
+        
+    }
+    
+    return output
+}
+
+
+console.log(stringToArray('aqrst,ukaei,ffooo'));
+console.log(stringToArray('qwer,tyui,asdf,ghjk'));
