d294ed2d9ab54aa6431118f4dbe6a0ff9eedf78b
fauzan_bintang <obin.aji@gmail.com>
2018-11-28 10:28:12 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-//your code here
+function stringToArray(str) {
+    str = str.split(',')
+    let result = []
+    str.forEach(e => {
+        result.push(e.split(''))
+    });
+    return result
+}
+
+console.log(stringToArray('aqrst,ukaei,ffooo'));
