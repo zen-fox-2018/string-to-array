95398d5215da9eaf38d11f7e322adea458f7b2ec
maitreya <bluespider529@gmail.com>
2018-11-28 14:26:23 +0700

    done string to array

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//your code here
+
+function stringToArray(input) {
+    // console.log(input)
+    let result = [];
+    let strSplitted = input.split(",");
+    for(let i = 0; i < strSplitted.length; i++) {
+        result.push([]);
+        for(let j = 0; j < strSplitted[i].length; j++) {
+            result[i].push(strSplitted[i][j]);
+        }
+    }
+    return result;
+}
+const str1 = "aqrst,ukaei,ffooo";
+const str2 = "qwer,tyui,asdf,ghjk";
+
+console.log(stringToArray(str1));
+console.log(stringToArray(str2));
